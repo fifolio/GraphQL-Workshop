a2a1a094d082a58e55527825cc5f98052ef47b3a
Firas <firasdabbabi@gmail.com>
2023-03-19 21:37:54 +0100

    version 2.5: Define type User{} in typeDefs.js

--- users/server/typeDefs.js
+++ users/server/typeDefs.js
@@ -3,5 +3,13 @@ const { gql } = require("apollo-server-express");
 module.exports = gql`
   type Query {
     doesItWork: Boolean!
+    getAllUsers: [User!]!
+  }
+  type User {
+    id: ID!
+    userName: String!
+    firstName: String!
+    favNumber: Int!
+    isActive: Boolean!
   }
 `;
