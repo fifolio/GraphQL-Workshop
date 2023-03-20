d116dcf9d9c09a605fc1c63c2b3dbcf0293e3ef9
Firas <firasdabbabi@gmail.com>
2023-03-20 21:06:40 +0100

    version 3.5: Setting up the Client side

---
+++ users/client/src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello Apollo</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ users/client/src/main.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+
+const client = new ApolloClient({
+  uri: "http://localhost:3000/graphql",
+  cache: new InMemoryCache(),
+});
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </React.StrictMode>
+);
