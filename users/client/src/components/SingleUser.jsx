6dca8adf3b7f6716bf13d30b5257027c1f08fa81
Firas <firasdabbabi@gmail.com>
2023-03-21 20:30:49 +0100

    version 4.5, in SingleUser.jsx, Create Delete, Edit, Save buttons

--- users/client/src/components/SingleUser.jsx
+++ users/client/src/components/SingleUser.jsx
@@ -1,13 +1,60 @@
-// import { useQuery } from "@apollo/client";
-// import queries from "../api/queries";
+import { useQuery } from "@apollo/client";
+import {useParams} from 'react-router-dom';
+import queries from "../api/queries";
 
 
-export default function SingleUser(props) {
-  // const {getUser} = queries
-  // const {loading, data, error} = useQuery(getUser)
+
+export default function SingleUser() {
+  const params = useParams();
+  const {id} = params;
+  const {getUser} = queries
+  const {loading, data, error} = useQuery(getUser, {
+    variables:{
+      userID: id
+    }
+  });
+
+const handleDeleteClicked = () => {
+  console.log("Clicked")
+}
+
   return (
     <>
-      <h1>User</h1>
+      <span>User Panel</span>
+      <table>
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Username</th>
+            <th>1st Name</th>
+            <th>2nd Name</th>
+            <th>Favorite Number</th>
+            <th>User Activision</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data && 
+          <tr>
+            <td>{data.getUser.id}</td>
+            <td>{data.getUser.userName}</td>
+            <td>{data.getUser.firstName}</td>
+            <td>{data.getUser.lastName}</td>
+            <td>{data.getUser.favNumber}</td>
+            <td>{data.getUser.isActive ? (<b className="Active">Active</b>) : (
+            <b className="NotActive">Not Active</b>)}
+        </td>
+      </tr>
+}
+        </tbody> 
+      </table>
+      {loading && <p>Loading User Information's ...</p>}
+      {error && <p>Error: {error.message}</p>}
+      <br />
+      <div>
+        <button onClick={handleDeleteClicked} style={{"background": "#DB192F"}}>Delete</button>
+        <button style={{"margin": "0 20px"}}>Edit</button>
+        <button style={{"background": "#00C75D"}}>Save</button>
+      </div>
     </>
   );
 }
