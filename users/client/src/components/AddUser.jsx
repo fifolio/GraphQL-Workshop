c8b3bc0b9d43c9226086b917044ef6ac32f986d4
Firas <firasdabbabi@gmail.com>
2023-03-21 22:25:38 +0100

    version 5.1, in AddUser.jsx, Create form state | Create HandleChange function

--- users/client/src/components/AddUser.jsx
+++ users/client/src/components/AddUser.jsx
@@ -1,31 +1,47 @@
+import { useState } from "react";
+
 export default function AddUser() {
+
+  
+  
+const [formValues, setFormValues] = useState({
+  userName: "",
+  firstName: "",
+  lastName: "",
+  favNumber: 0,
+  isActive: false,  
+})
+const handleChange = (event) => {
+  console.log(event.target)
+};
+
   return (
     <>
       <span>Add New User</span>
       <form>
         <div>
           <label htmlFor="userName">Username: </label>
-          <input type="text" name="firstName" />
+          <input type="text" name="userName" value={formValues.userName} onChange={handleChange} />
         </div>
 
         <div>
           <label htmlFor="firstName">Firas Name: </label>
-          <input type="text" name="firstName" />
+          <input type="text" name="firsstName" value={formValues.firstName} onChange={handleChange} />
         </div>
 
         <div>
           <label htmlFor="lastName">Last Name: </label>
-          <input type="text" name="firstName" />
+          <input type="text" name="lastName" value={formValues.lastName} onChange={handleChange} />
         </div>
 
         <div>
           <label htmlFor="favNumber">Fav Number: </label>
-          <input type="number" name="favNumber" />
+          <input type="number" name="favNumber" value={formValues.favNumber} onChange={handleChange} />
         </div>
 
         <div>
           <label htmlFor="isActive">Is User Active? </label>
-          <input type="checkbox" name="isActive" />
+          <input type="checkbox" name="isActive" value={formValues.isActive} onChange={handleChange} />
         </div>
 
         <div>
